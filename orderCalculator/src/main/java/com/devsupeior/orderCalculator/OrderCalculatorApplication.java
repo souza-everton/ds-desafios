d8f1bb043ece481d02f1b76627e8585544924103
Everton Souza <eve.souzalemos@gmail.com>
2024-11-16 12:17:43 -0300

    Desafio criado

---
+++ orderCalculator/src/main/java/com/devsupeior/orderCalculator/OrderCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.devsupeior.orderCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderCalculatorApplication.class, args);
+	}
+
+}
---
+++ orderCalculator/src/test/java/com/devsupeior/orderCalculator/OrderCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsupeior.orderCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

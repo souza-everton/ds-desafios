07080244d7bda84a28c0826849e20cb5723045e2
Everton Souza <eve.souzalemos@gmail.com>
2024-11-21 23:45:20 -0300

    Project Created

---
+++ desafioevento/src/main/java/com/devsuperior/desafioevento/DesafioeventoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafioevento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioeventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioeventoApplication.class, args);
+	}
+
+}
---
+++ desafioevento/src/test/java/com/devsuperior/desafioevento/DesafioeventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafioevento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioeventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

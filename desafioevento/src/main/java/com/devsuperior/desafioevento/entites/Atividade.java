679215f5bf69ad4fcb6b9f3da534e25c44e67789
Everton Souza <eve.souzalemos@gmail.com>
2024-11-22 23:47:09 -0300

    Atividade|Bloco|Participante Entities

---
+++ desafioevento/src/main/java/com/devsuperior/desafioevento/entites/Atividade.java
@@ -0,0 +1,66 @@
+package com.devsuperior.desafioevento.entites;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String nome;
+	@Column(columnDefinition = "TEXT")
+	private String descricao;
+	private Double preco;
+
+	public Atividade() {
+
+	}
+
+	public Atividade(Integer id, String nome, String descricao, Double preco) {
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.preco = preco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+}
---
+++ desafioevento/src/main/java/com/devsuperior/desafioevento/entites/Participante.java
@@ -0,0 +1,57 @@
+package com.devsuperior.desafioevento.entites;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_participante")
+public class Participante {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String nome;
+	@Column(unique = true)
+	private String email;
+	
+	public Participante() {
+		
+	}
+
+	public Participante(Integer id, String nome, String email) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}

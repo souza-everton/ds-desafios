7d6d6821209fedc5c8ef41e48505da255df2d958
Everton Souza <eve.souzalemos@gmail.com>
2024-12-10 10:20:34 -0300

    Project Created

---
+++ desafiocliente/src/main/java/com/devsuperior/desafiocliente/DesafioclienteApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafiocliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioclienteApplication.class, args);
+	}
+
+}
---
+++ desafiocliente/src/test/java/com/devsuperior/desafiocliente/DesafioclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafiocliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
